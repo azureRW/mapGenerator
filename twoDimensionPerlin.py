d500ee945117ccfe9531f2d4d1d3397694a35a63
azureRW <joker4202@gmail.com>
2023-03-29 10:03:16 +0800

    smooth
    recorrect the method smooth curve

--- twoDimensionPerlin.py
+++ twoDimensionPerlin.py
@@ -6,31 +6,28 @@ from matplotlib import cm
 def blending(t):
     return 6 * (t ** 5) - 15 * (t ** 4) + 10 * (t ** 3)
 
-random2D = np.random.randint(-256, 256, size=(12, 12))
 
-def mapi(xx,yy,mox,moy):
-    return random2D[xx][yy] *(
-        blending((mox**2+moy**2)/2**0.5)
-    )
-    
-    
-    
-
-def perlin(x, y):
+def perlin(x, y,matrix):
     x1 = np.floor(x).astype(int)
     x2 = np.floor(x+1).astype(int)
     y1 = np.floor(y).astype(int)
     y2 = np.floor(y+1).astype(int)
-    return mapi(x1, y1, x2-x, y2-y) + mapi(x2, y1, x-x1, y2-y) + mapi(x1, y2, x2-x, y-y1) - mapi(x2, y2, x-x1, y-y1)
-
+    return (
+        (matrix[x1][y1]*blending(x2-x)+matrix[x2][y1]*blending(x-x1))*blending(y2-y)
+        +
+        (matrix[x1][y2]*blending(x2-x)+matrix[x2][y2]*blending(x-x1))*blending(y-y1)
+    )
+    
+    
 unit=100
+random2D = np.random.randint(-256, 256, size=(12, 12))
 xArray = np.linspace(0, 10, unit)
 yArray = np.linspace(0, 10, unit)
 X, Y = np.meshgrid(xArray, yArray)
 Z = np.empty((unit, unit))
 for i, x in enumerate(xArray):
     for j, y in enumerate(yArray):
-        Z[i,j] = perlin(x, y)
+        Z[i,j] = perlin(x, y,random2D)
 
 fig = plt.figure()
 ax = plt.axes(projection='3d')
