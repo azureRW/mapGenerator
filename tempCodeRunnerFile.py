35c8f0a354028d0069bb5a519df6f4df85d354fa
azureRW <joker4202@gmail.com>
2023-03-29 16:54:24 +0800

    normalize Z matrix

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+
+def perlin(x, y,matrix):
--- twoDimensionPerlin.py
+++ twoDimensionPerlin.py
@@ -2,6 +2,8 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import cm
+import cv2
+
 
 def blending(t):
     return 6 * (t ** 5) - 15 * (t ** 4) + 10 * (t ** 3)
@@ -17,18 +19,30 @@ def perlin(x, y,matrix):
         +
         (matrix[x1][y2]*blending(x2-x)+matrix[x2][y2]*blending(x-x1))*blending(y-y1)
     )
-    
-    
-unit=100
-random2D = np.random.randint(-256, 256, size=(12, 12))
-xArray = np.linspace(0, 10, unit)
-yArray = np.linspace(0, 10, unit)
-X, Y = np.meshgrid(xArray, yArray)
-Z = np.empty((unit, unit))
-for i, x in enumerate(xArray):
-    for j, y in enumerate(yArray):
-        Z[i,j] = perlin(x, y,random2D)
 
+def noise(frequentIndictor):
+    unit=frequentIndictor*100
+    random2D = np.random.uniform(-5, 5, size=(np.floor(unit/10).astype(int)+2, np.floor(unit/10).astype(int)+2))
+    xArray = np.linspace(0, unit/10, unit)
+    yArray = np.linspace(0, unit/10, unit)
+    X, Y = np.meshgrid(xArray, yArray)
+    Z = np.empty((unit, unit))
+    for i, x in enumerate(xArray):
+        for j, y in enumerate(yArray):
+            Z[i,j] = perlin(x, y,random2D) 
+    if frequentIndictor==1:
+        return X,Y,Z
+    else:
+        return cv2.resize(Z, (100, 100), interpolation=cv2.INTER_AREA)
+
+    
+    
+
+
+Z=noise(8)
+xArray = np.linspace(0, 10, 100)
+yArray = np.linspace(0, 10, 100)
+X, Y = np.meshgrid(xArray, yArray)
 fig = plt.figure()
 ax = plt.axes(projection='3d')
 ax.plot_surface(X, Y, Z, cmap = cm.gist_earth) 
